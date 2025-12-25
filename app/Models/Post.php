bb8d54f35e48e855fdc2938a04459aa0054435f4
Charlotte -Schroer <charlotte.schroer@student.ehb.be>
2025-12-25 18:20:32 +0100

    Models for Forum

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,10 +1,30 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'topic_id',
+        'user_id',
+        'content',
+    ];
+
+    public function topic(): BelongsTo
+    {
+        return $this->belongsTo(Topic::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Topic extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'user_id',
         'title',
@@ -28,5 +30,29 @@ class Topic extends Model
     public function tags(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
     {
         return $this->belongsToMany(Tag::class);
+    }
+
+    public function getReplyCountAttribute(): int
+    {
+        return $this->posts()->count();
+    }
+
+    public function latestPost()
+    {
+        return $this->posts()->latest()->first();
+    }
+
+    public function scopeWithTag($query, $tag)
+    {
+        return $query->whereHas('tags', function ($query) use ($tag) {
+            $query->where('tags.id', $tag);
+        });
+    }
+
+    public function scopeSearch($query, $search)
+    {
+        return $query->where('title', 'like', '%' . $search . '%')
+            ->orWhere('content', 'like', '%' . $search . '%')
+            ->orWhere('movie_title', 'like', '%' . $search . '%');
     }
 }
