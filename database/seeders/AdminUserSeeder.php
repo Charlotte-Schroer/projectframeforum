e6268386610089b45f15f0824903b3ded4490f96
Charlotte -Schroer <charlotte.schroer@student.ehb.be>
2025-12-25 18:31:01 +0100

    Fix problem userseed

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -16,13 +16,15 @@ class AdminUserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
-            'name' => 'Admin',
-            'username' => 'admin',
-            'email' => 'admin@ehb.be',
-            'password' => Hash::make('Password!321'),
-            'is_admin' => true,
-            'email_verified_at' => now(),
-        ]);
+        User::updateOrCreate(
+            ['username' => 'admin'],
+            [
+                'name' => 'Admin',
+                'email' => 'admin@ehb.be',
+                'password' => Hash::make('Password!321'),
+                'is_admin' => true,
+                'email_verified_at' => now(),
+            ]
+        );
     }
 }
