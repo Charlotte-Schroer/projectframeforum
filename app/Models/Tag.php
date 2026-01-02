2f87d43802bcc20f71675274ac93b00231899531
Charlotte -Schroer <charlotte.schroer@student.ehb.be>
2026-01-02 14:41:22 +0100

    Update Tag.php

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,14 +4,22 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
+    use HasFactory;
     protected $fillable = ['name'];
 
-    public function topics(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+    public function topics(): BelongsToMany
     {
         return $this->belongsToMany(Topic::class);
+    }
+
+    public function getTopicCountAttribute(): int
+    {
+        return $this->topics()->count();
     }
 }
